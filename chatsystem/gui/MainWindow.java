4d175ad03c377f7fa5923724d27b23918920691c
Laura <lauraburlonroux@hotmail.fr>
2019-12-03 17:05:49 +0100

    continue MW

--- chatsystem/gui/MainWindow.java
+++ chatsystem/gui/MainWindow.java
@@ -41,20 +41,39 @@ public class MainWindow extends JFrame{
 		
 		//About US
 		JPanel panUs = new JPanel();
+		panUs.setBackground(Color.BLUE); 
+		Box BoxAboutUsMain = Box.createVerticalBox();
+		panUs.add(BoxAboutUsMain, BorderLayout.NORTH);
+
 		Box BoxAboutUs = Box.createVerticalBox();
-		panUs.add(BoxAboutUs);
+		BoxAboutUsMain.add(BoxAboutUs, BorderLayout.NORTH);
+
 		panUs.setBackground(Color.white);
 		JLabel padding1 = new JLabel("\n");
 		BoxAboutUs.add(padding1);
 		ImageIcon icon = new ImageIcon("../resources/images/user.png");
 		JLabel image = new JLabel(icon); 
+		image.setAlignmentX(Component.CENTER_ALIGNMENT);
 		BoxAboutUs.add(image);
-		JLabel padding2 = new JLabel("\n");
+		JLabel padding2 = new JLabel("\n\n");
 		BoxAboutUs.add(padding2);
 		JLabel textName = new JLabel("Paul Hochon");
-		textName.setFont(new java.awt.Font(Font.SERIF,Font.BOLD,15));
+		textName.setFont(new java.awt.Font("CALIBRI",Font.BOLD,18));
+		textName.setAlignmentX(Component.CENTER_ALIGNMENT);
 		BoxAboutUs.add(textName);
 		
+
+		Box BoxAboutUs2 = Box.createHorizontalBox();
+		BoxAboutUsMain.add(BoxAboutUs2, BorderLayout.SOUTH);
+		ImageIcon icon2 = new ImageIcon("../resources/images/msg.png");
+		JLabel msg = new JLabel(icon2); 
+		BoxAboutUs2.add(msg);
+		ImageIcon icon3 = new ImageIcon("../resources/images/option.png");
+		JLabel option= new JLabel(icon3); 
+		BoxAboutUs2.add(option);
+		
+
+
 
    	 	JPanel panUsers = new JPanel();
     		panUsers.setBackground(Color.white);
@@ -69,11 +88,11 @@ public class MainWindow extends JFrame{
 		//SPLITS
     		s1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, panUs, panUsers);
 		s1.setDividerSize(2);
-		s1.setDividerLocation(200);
+		s1.setDividerLocation(180);
  		s1.setEnabled( false );
 
 		s2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, s1, panMsg);
-    		s2.setDividerLocation(300);
+    		s2.setDividerLocation(280);
 		s2.setDividerSize(7);
 		s2.setEnabled( false );
 
