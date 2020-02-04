23f923860f2f0273c2b037ac8d1fd881a05cff8f
Michaud Aurelien <a_michau@insa-08135>
2020-02-04 16:12:19 +0100

    setup script finished

--- setup.sh
+++ setup.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
+configFilePath="./configs/config.cfg"
+witnessFilePath="./resources/witness"
+
 echo "Creating 'jar' directory..."
 mkdir jar/ &> /dev/null
-echo "Compiling from sources..."
-make &> /dev/null && cd ./build && jar -cvf Aura.jar ./chatsystem/ &> /dev/null && mv ./Aura.jar ../jar/;
+echo "Compiling from source..."
+make &> /dev/null && cd ./build && echo "Creating jar file..." && jar -cvf Aura.jar ./chatsystem/ &> /dev/null && mv ./Aura.jar ../jar/;
+cd ../
 
-passwd=""
-passwd2="a"
+if [ ! -r "$witnessFilePath" ] ; then
 
-while [ "$passwd" != "$passwd2" ] ; do
-	echo -n "New password: "
-	read -s passwd
-	echo ""
-	echo -n "Comfirm new password: "
-	read -s passwd2
-	echo ""
+	echo -n "[*] No account was found, do you want to create one ? [yes] : "
 
-	if [ "$passwd" != "$passwd2" ] ; then
-		echo "Wrong password, please do it again"
-	fi
-done
+	read answer
 
+	case "$answer" in
+		no)
+			;;
+		*)
+			passwd=""
+			passwd2="a"
 
-cd ../jar/ && java -cp ./Aura.jar chatsystem.util.EncryptionHandler "$passwd" > ../resources/witness
+			while [ "$passwd" != "$passwd2" ] ; do
+				echo -n "New password: "
+				read -s passwd
+				echo ""
+				echo -n "Comfirm new password: "
+				read -s passwd2
+				echo ""
+
+				if [ "$passwd" != "$passwd2" ] ; then
+					echo "Wrong password, please do it again"
+				fi
+			done
+
+			cd ./jar && java -cp ./Aura.jar chatsystem.util.EncryptionHandler "$passwd" > ../resources/witness
+			cd ../
+			;;
+	esac
+
+fi
 
 echo "Removing compiled files..."
-cd ../ && make clean
+make clean
 
 echo "Setup done."
 
